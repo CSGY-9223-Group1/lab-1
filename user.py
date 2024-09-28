ad9cb0e90f486b45b2046cecef56101f30d6bc71
ss14635 <ss14635@nyu.edu>
2024-09-28 15:30:03 -0400

    Refactor code with Black Code Formatter

--- user.py
+++ user.py
@@ -1,17 +1,18 @@
 import json
 
+
 class User:
     def __init__(self, userid, name, token):
         self.userid = userid
         self.name = name
         self.token = token
-        
+
     def get_userid(self):
         return self.userid
-    
+
     def get_name(self):
         return self.name
-    
+
     def get_token(self):
         return self.token
 
