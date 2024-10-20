207fb14662a240c670c5d93fbe9e904c7e3c32e6
ss14635 <ss14635@nyu.edu>
2024-10-20 16:06:37 -0400

    Run the app in debug mode by default so troubleshooting is easier

--- main.py
+++ main.py
@@ -123,4 +123,4 @@ def update_note(current_user):
     return '{"status": "success"}'
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5005)
+    app.run(host="0.0.0.0", port=5005, debug=True)
