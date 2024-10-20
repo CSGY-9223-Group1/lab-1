cdd3ca4c650b7fe28bfed8df57300dc1ab916c59
ss14635 <ss14635@nyu.edu>
2024-10-20 17:43:03 -0400

    Apply black formatting

--- note.py
+++ note.py
@@ -12,14 +12,18 @@ class Note:
     def get_note_id(self):
         return self.note_id
 
+
     def get_owner_id(self):
         return self.owner_id
+
 
     def get_note(self):
         return self.note
 
+
     def set_note(self, upd_note):
         self.note = upd_note
+
 
     def toJson(self):
         return json.dumps(self, default=lambda o: o.__dict__)
