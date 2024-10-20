a80231a1c3955ab2cc8429bec4b5ee89a2ca1d8c
ss14635 <ss14635@nyu.edu>
2024-10-20 15:48:40 -0400

    Test removing changes to see if scanned lines changes

--- main.py
+++ main.py
@@ -122,23 +122,5 @@ def update_note(current_user):
             return '{"status": "note not found"}'
     return '{"status": "success"}'
 
-@app.route("/example_bad")
-def example_bad():
-    rfs_header = request.args["rfs_header"]
-    response = Response()
-    custom_header = "X-MyHeader-" + rfs_header
-    # BAD: User input is used as part of the header name.
-    response.headers[custom_header] = "HeaderValue" 
-    return response
-
-@app.route("/example_good")
-def example_bad():
-    rfs_header = request.args["rfs_header"]
-    response = Response()
-    custom_header = "X-MyHeader-" + rfs_header.replace("\n", "").replace("\r","").replace(":","")
-    # GOOD: Line break characters are removed from the input.
-    response.headers[custom_header] = "HeaderValue" 
-    return response
-
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5005)
